a30004ae486efe95659f1b88eb91ce9489ba82fc
PattharananZ <42967288+PattharananZ@users.noreply.github.com>
2021-05-04 13:35:08 +0700

    Send work to dollydell

---
+++ worklify/src/pages/Note.js
@@ -0,0 +1,23 @@
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Zoom from "@material-ui/core/Zoom";
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <Zoom in={true}>
+        <button onClick={handleClick}>
+          <DeleteIcon />
+        </button>
+      </Zoom>
+    </div>
+  );
+}
+
+export default Note;
