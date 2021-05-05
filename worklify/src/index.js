a06e1c491946ea63599daf3955b4681d8966a03e
RewiKaze <lnwkazeazelass@hotmail.co.th>
2021-05-05 10:29:31 +0700

    Update index.js

--- worklify/src/index.js
+++ worklify/src/index.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
-import './index.css';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./index.css";
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './contexts/AuthContext';
-
-import Navbar from './components/Navbar/Navbar';
-import ProjectNav from "./components/Navbar/ProjectNav"
-
+import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "./contexts/AuthContext";
+import Loading from "./components/Loading";
+// import Navbar from "./components/Navbar/Navbar";
+// import ProjectNav from "./components/Navbar/ProjectNav";
+const Navbar = React.lazy(() => import("./components/Navbar/Navbar"));
+const ProjectNav = React.lazy(() => import("./components/Navbar/ProjectNav"));
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <Router>
-        <Switch>
-        <Route path ="/project">
-             <ProjectNav/>
-          </Route>
-          <Route strict path="/">
-            <Navbar />
-          </Route>
-        </Switch>
-        {/* <TaskBoard/> */}
-      </Router>
+      <Suspense fallback={<Loading />}>
+        <Router>
+          <Switch>
+            <Route path="/project">
+              <ProjectNav />
+            </Route>
+            <Route strict path="/">
+              <Navbar />
+            </Route>
+          </Switch>
+          {/* <TaskBoard/> */}
+        </Router>
+      </Suspense>
     </AuthProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
